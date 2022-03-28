b4941d31cc0f57e7278abd98d734c7486fe4bca9
CalebeSG <calebeguimaraes10@gmail.com>
2022-03-28 19:32:36 -0300

    implemented pricing logic

--- script.js
+++ script.js
@@ -1,10 +1,68 @@
 const slider = document.getElementById('traffic');
 const fill = document.querySelector('.fill');
+const displayPageView = document.querySelector('.display--view');
+const typePayment = document.getElementById('payment');
+const displayPrice = document.querySelector('.price span');
+const displayTypePayment = document.querySelector('.price small');
 
-const moveBar = function () {
-  fill.style.width = `${slider.value}%`;
+const playmentPlans = [
+  {
+    value: 8,
+    quantity: 10,
+  },
+  {
+    value: 12,
+    quantity: 50,
+  },
+  {
+    value: 16,
+    quantity: 100,
+  },
+  {
+    value: 24,
+    quantity: 500,
+  },
+  {
+    value: 36,
+    quantity: 1,
+  },
+];
+
+let currentPlan = playmentPlans[0];
+let yearPlan = false;
+displayTypePayment.textContent = '/ month';
+
+const applyDiscount = function () {
+  const totalAnnual = currentPlan.value * 12 * 0.25;
+
+  displayPageView.textContent = currentPlan.quantity;
+  displayPrice.textContent = totalAnnual.toFixed(2);
 };
 
-moveBar();
+const calculate = function () {
+  if (yearPlan) return applyDiscount();
 
-slider.addEventListener('input', moveBar);
+  displayPageView.textContent = currentPlan.quantity;
+  displayPrice.textContent = currentPlan.value.toFixed(2);
+};
+
+const moveBar = function () {
+  const sliderPosition = slider.value;
+
+  fill.style.width = `${sliderPosition * 25}%`;
+
+  currentPlan = playmentPlans[sliderPosition];
+};
+
+slider.addEventListener('input', function () {
+  moveBar();
+  calculate();
+});
+
+typePayment.addEventListener('change', function () {
+  yearPlan = !yearPlan;
+  displayTypePayment.textContent = yearPlan ? '/ year' : '/ month';
+  calculate();
+});
+
+moveBar();
